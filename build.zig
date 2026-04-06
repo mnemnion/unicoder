bf455e4fbe3057de3f182c40086ab550a3a97e55
Sam Atman <atmanistan@gmail.com>
2026-04-06 10:39:32 -0400

    easteregg

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const plan9_module = b.addModule("plan9", .{
-        .root_source_file = b.path("plan9.zig"),
+        .root_source_file = b.path("src/plan9.zig"),
         .target = target,
         .optimize = optimize,
     });
